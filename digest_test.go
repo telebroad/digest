b7a0bca2ff7159157dc920153273b2cbdf745e42
isaac <isaac@telebroad.com>
2022-04-14 16:30:52 -0400

    fixed requireTLS error

--- digest_test.go
+++ digest_test.go
@@ -1,18 +1,30 @@
 package digest
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
+
+type vars struct {
+	method string
+	host   string
+	uri    string
+	user   string
+	pass   string
+}
+
+var theVars = vars{
+	method: "POST",
+	host:   "https://www.example.com",
+	uri:    "/some/route",
+	user:   "my-username",
+	pass:   "my-password",
+}
 
 func TestDigest(t *testing.T) {
+	fmt.Printf(">%+v\n", theVars)
+	token, err := Token(theVars.method, theVars.host, theVars.uri, theVars.user, theVars.pass, false)
 
-	var (
-		method = "POST"
-		host   = "https://www.example.com"
-		uri    = "/some/route"
-		user   = "my-username"
-		pass   = "my-password"
-	)
-
-	token, err := Token(method, host, uri, user, pass, false)
 	if err != nil {
 		t.Errorf("token failed: %s", err.Error())
 	}
